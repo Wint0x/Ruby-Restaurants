ba3fba3cb5082989af82579140ec5bfbd478a4be
Zamius <costi19000@gmail.com>
2024-03-26 01:22:18 +0200

    Continued working on the registration/login system, added a logout function, working registration function and login/logout checks to determine if the user is already logged in or not

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 		#Authenticate user credentials
 		if !!@user && @user.authenticate(params[:password])
 			session[:user_id] = @user.id
-			redirect_to user_path
+			redirect_to root_path
 		else
 			#Error on failure
 			message = "Wrong username or password!"
@@ -13,4 +13,16 @@ class SessionsController < ApplicationController
 			puts "Not found"
 		end
 	end
+
+	def destroy
+	    if session[:user_id]
+	      # Clear the user's session
+	      session[:user_id] = nil
+	      # Display logout message
+	      flash[:notice] = "Successfully logged out"
+	    
+	    else
+	    	flash[:error] = "Not logged-in!"
+	    end
+  	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id
-			redirect_to root_path
+			redirect_to login_url, notice: "Registration Succesful! You may now login..."
 		else
 			render :new
 		end
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:username, :password)
+		params.require(:user).permit(:username, :password, :password_confirmation)
 	end
 	
 end
